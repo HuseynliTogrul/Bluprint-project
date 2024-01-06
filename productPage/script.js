986a6bed9e17135504b002141193b9b7074d8ca2
HuseynliTogrul <toghrulih@code.edu.az>
2024-01-06 23:41:22 +0400

    bluprint project continue

--- productPage/script.js
+++ productPage/script.js
@@ -1,72 +1,81 @@
 "use strict";
 
-const productCategory = document.getElementById("productCategory")
-const productName = document.getElementById("productName")
-const productImage = document.getElementById("productImage")
-const productPrice = document.getElementById("productPrice")
-const productColor = document.getElementById("productColor")
+const productCategory = document.getElementById("productCategory");
+const productName = document.getElementById("productName");
+const productImage = document.getElementById("productImage");
+const productPrice = document.getElementById("productPrice");
+const productColor = document.getElementById("productColor");
 
-const decreaseEL = document.getElementById("decrease")
-const counterEl = document.getElementById("counter")
-const increaseEl = document.getElementById("increase")
+const decreaseEL = document.getElementById("decrease");
+const counterEl = document.getElementById("counter");
+const increaseEl = document.getElementById("increase");
 
-const addToCart = document.getElementById("addToCart")
+const addToCart = document.getElementById("addToCart");
 
-const sizesListLiEl = document.querySelectorAll("#sizesList li")
-const sizesListDatas = [...sizesListLiEl].map((item) => item.dataset.sizes)
-const sizesListBtnEl = document.querySelectorAll("#sizesList button")
+const sizesListLiEl = document.querySelectorAll("#sizesList li");
+const sizesListDatas = [...sizesListLiEl].map((item) => item.dataset.sizes);
+const sizesListBtnEl = document.querySelectorAll("#sizesList button");
+
+const cartItems = [];
 
 let count = 1;
 counterEl.value = count;
 
-const { category, name, image, price, color, sizes } = JSON.parse(localStorage.getItem("product"))
+const { category, name, image, price, color, sizes } = JSON.parse(
+  localStorage.getItem("product")
+);
 
-const pName = name.split(" ").map((name) => name[0].toUpperCase() + name.slice(1)).join(" ")
+const pName = name
+  .split(" ")
+  .map((name) => name[0].toUpperCase() + name.slice(1))
+  .join(" ");
 
-productCategory.textContent = category
-productName.textContent = pName
-productPrice.textContent = `${price} AZN`
-productColor.classList.add(`${color === "black" ? "!bg-black" : `bg-${color}-500`}`)
-productImage.src = image
+productCategory.textContent = category;
+productName.textContent = pName;
+productPrice.textContent = `${price} AZN`;
+productColor.classList.add(
+  `${color === "black" ? "!bg-black" : `bg-${color}-500`}`
+);
+productImage.src = image;
 
 decreaseEL.addEventListener("click", () => {
-    if (count > 1) {
-        count--;
-    }
-    counterEl.value = count
-})
+  if (count > 1) {
+    count--;
+  }
+  counterEl.value = count;
+});
 
 increaseEl.addEventListener("click", () => {
-    if (count < 10) {
-        count++;
-    }
-    counterEl.value = count
-})
+  if (count < 10) {
+    count++;
+  }
+  counterEl.value = count;
+});
 
 addToCart.addEventListener("click", () => {
-    const fetchedCartItems = JSON.parse(localStorage.getItem("cartItems"))
+  const fetchedCartItems = JSON.parse(localStorage.getItem("cartItems"));
 
-    const addedProduct = {
-        name: pName,
-        price: price,
-        category: category,
-        color: color,
-        image: image,
-        count: count
-    }
+  const addedProduct = {
+    name: pName,
+    price: price,
+    category: category,
+    color: color,
+    image: image,
+    count: count,
+  };
 
-    if (!fetchedCartItems) {
-        cartItems.push(addedProduct)
-    } else {
-        cartItems.push(...fetchedCartItems)
-    }
+  if (!fetchedCartItems) {
+    cartItems.push(addedProduct);
+  } else {
+    cartItems.push(...fetchedCartItems);
+  }
 
-    localStorage.setItem("cartItems", JSON.stringify(cartItems))
-})
+  localStorage.setItem("cartItems", JSON.stringify(cartItems));
+});
 
 sizesListDatas.forEach((size, index) => {
-    if (!sizes.includes(size)){
-        sizesListLiEl[index].classList.add("opacity-40");
-        sizesListBtnEl[index].classList.add("cursor-none-allowed");
-    }
+  if (!sizes.includes(size)) {
+    sizesListLiEl[index].classList.add("opacity-40");
+    sizesListBtnEl[index].classList.add("cursor-none-allowed");
+  }
 });
--- script.js
+++ script.js
@@ -2,110 +2,117 @@
 
 import products from "./data.js";
 
-const filterBtn = document.querySelectorAll(".filterBtn")
-const filterList = document.querySelectorAll(".filterList")
-const chevronIcon = document.querySelectorAll(".chevronIcon")
+const filterBtn = document.querySelectorAll(".filterBtn");
+const filterList = document.querySelectorAll(".filterList");
+const chevronIcon = document.querySelectorAll(".chevronIcon");
 
-const categoryList = document.querySelectorAll("#categoryList li")
-const colorList = document.querySelectorAll("#colorList li")
-const sizeList = document.querySelectorAll("#sizeList li")
+const categoryList = document.querySelectorAll("#categoryList li");
+const colorList = document.querySelectorAll("#colorList li");
+const sizeList = document.querySelectorAll("#sizeList li");
 
-const colorListDots = document.querySelectorAll("#colorList li span")
+const colorListDots = document.querySelectorAll("#colorList li span");
 
-const productsCount = document.getElementById("products-count")
+const productsCount = document.getElementById("products-count");
 
-const productsEl = document.getElementById("products")
+const productsEl = document.getElementById("products");
 
-const filteredCategories = []
-const filteredColors = []
-const filteredSizes = []
+const filteredCategories = [];
+const filteredColors = [];
+const filteredSizes = [];
 
 filterBtn.forEach((btn, index) => {
-    btn.addEventListener("click", () => {
-        filterList[index].classList.toggle("hidden")
-        chevronIcon[index].classList.toggle("-rotate-180")
-    })
+  btn.addEventListener("click", () => {
+    filterList[index].classList.toggle("hidden");
+    chevronIcon[index].classList.toggle("-rotate-180");
+  });
 });
 
 categoryList.forEach((item, index) => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("font-black")
+  item.addEventListener("click", () => {
+    item.classList.toggle("font-black");
 
-        const { category } = item.dataset;
+    const { category } = item.dataset;
 
-        if (filteredCategories.includes(category)) {
-            const indexOfCategory = filteredCategories.indexOf(category)
-            filteredCategories.splice(indexOfCategory, 1)
-        } else {
-            filteredCategories.push(category)
-        }
-        displayProducts()
-    })
+    if (filteredCategories.includes(category)) {
+      const indexOfCategory = filteredCategories.indexOf(category);
+      filteredCategories.splice(indexOfCategory, 1);
+    } else {
+      filteredCategories.push(category);
+    }
+    displayProducts();
+  });
 });
 
 colorList.forEach((item, index) => {
-    item.addEventListener("click", () => {
-        colorListDots[index].classList.toggle("hidden")
+  item.addEventListener("click", () => {
+    colorListDots[index].classList.toggle("hidden");
 
-        const { color } = item.dataset;
+    const { color } = item.dataset;
 
-        if (filteredColors.includes(color)) {
-            const indexOfColor = filteredColors.indexOf(color)
-            filteredColors.splice(indexOfColor, 1)
-        } else {
-            filteredColors.push(color)
-        }
+    if (filteredColors.includes(color)) {
+      const indexOfColor = filteredColors.indexOf(color);
+      filteredColors.splice(indexOfColor, 1);
+    } else {
+      filteredColors.push(color);
+    }
 
-        displayProducts()
-        // console.log(filteredColors);
-    })
+    displayProducts();
+    // console.log(filteredColors);
+  });
 });
 
 sizeList.forEach((item, index) => {
-    item.addEventListener("click", () => {
-        item.classList.toggle("bg-black")
-        item.classList.toggle("text-white")
+  item.addEventListener("click", () => {
+    item.classList.toggle("bg-black");
+    item.classList.toggle("text-white");
 
-        const { size } = item.dataset;
+    const { size } = item.dataset;
 
-        if (filteredSizes.includes(size)) {
-            const indexOfSize = filteredSizes.indexOf(size)
-            filteredSizes.splice(indexOfSize, 1)
-        } else {
-            filteredSizes.push(size)
-        }
+    if (filteredSizes.includes(size)) {
+      const indexOfSize = filteredSizes.indexOf(size);
+      filteredSizes.splice(indexOfSize, 1);
+    } else {
+      filteredSizes.push(size);
+    }
 
-        displayProducts()
-        // console.log(filteredSizes);
-    })
-})
+    displayProducts();
+    // console.log(filteredSizes);
+  });
+});
 
 const displayProducts = () => {
-    const filteredProducts = products.filter((product) => {
-        if (filteredCategories.length === 0 &&
-            filteredColors.length === 0 &&
-            filteredSizes.length === 0
-        ) {
-            return true;
-        }
+  const filteredProducts = products.filter((product) => {
+    if (
+      filteredCategories.length === 0 &&
+      filteredColors.length === 0 &&
+      filteredSizes.length === 0
+    ) {
+      return true;
+    }
 
+    const categoryCondition =
+      filteredCategories.length === 0 ||
+      filteredCategories.includes(product.category);
 
-        const categoryCondition = filteredCategories.length === 0 || filteredCategories.includes(product.category)
+    const colorCondition =
+      filteredColors.length === 0 || filteredColors.includes(product.color);
 
-        const colorCondition = filteredColors.length === 0 || filteredColors.includes(product.color)
+    const sizeCondition =
+      filteredSizes.length === 0 ||
+      filteredSizes.every((size) => product.sizes.includes(size));
 
-        const sizeCondition = filteredSizes.length === 0 || filteredSizes.every((size) => product.sizes.includes(size));
+    return categoryCondition && colorCondition && sizeCondition;
+  });
 
-        return categoryCondition && colorCondition && sizeCondition
-    })
+  productsEl.innerHTML = "";
 
-    productsEl.innerHTML = ""
+  filteredProducts.forEach((product) => {
+    const pName = product.name
+      .split(" ")
+      .map((name) => name[0].toUpperCase() + name.slice(1))
+      .join(" ");
 
-    filteredProducts.forEach((product) => {
-
-        const pName = product.name.split(" ").map((name) => name[0].toUpperCase() + name.slice(1)).join(" ");
-
-        productsEl.innerHTML += `<div class="col-span-4 cursor-pointer productItems">
+    productsEl.innerHTML += `<div class="col-span-4 cursor-pointer productItem">
                                 <a href="./productPage/product.html">
                                 <div class="mb-4 border border-neutral-200 rounded-lg">
                                     <img
@@ -125,20 +132,17 @@ const displayProducts = () => {
                                     </h1>
                                 </div>
                                 </a>
-                            </div>`
+                            </div>`;
 
-        const productItems = document.querySelectorAll(".productItem")
-
-        productItems.forEach((productItem, index) => {
-            productItem.addEventListener("click", () => {
-                const selectedProduct = filteredProducts[index];
-
-                localStorage.setItem("product", JSON.stringify(selectedProduct))
-            })
-        });
+    const productItems = document.querySelectorAll(".productItem");
+    productItems.forEach((productItem, index) => {
+      productItem.addEventListener("click", () => {
+        const selectedProduct = filteredProducts[index];
+        localStorage.setItem("product", JSON.stringify(selectedProduct));
+      });
     });
+  });
+  productsCount.textContent = filteredProducts.length;
+};
 
-    productsCount.textContent = filteredProducts.length;
-}
-
-displayProducts()
+displayProducts();
